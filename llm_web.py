7714dfbb03348c26bd4f6d1a2ab85b0df42d5db6
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2024-10-15 16:01:47 +0200

    feat: --inject_script and moved injectScript to separate file

--- llm_web.py
+++ llm_web.py
@@ -24,6 +24,7 @@ def parse_arguments():
     parser.add_argument("--base_url", type=str, default="http://localhost:5000/", help="Base URL for the server")
     parser.add_argument("--base_prompt", type=str, default="prompts/base_prompt.txt", help="Path to the base prompt file")
     parser.add_argument("--css_prompt", type=str, default="prompts/css_prompt.txt", help="Path to the base prompt file")
+    parser.add_argument("--inject_script", type=str, default="injectScript.js", help="Path to the script to be injected into all webpages.")
     return parser.parse_args()
 
 args = parse_arguments()
@@ -32,6 +33,8 @@ with open(args.base_prompt, "r") as file:
     BASE_PROMPT = file.read()
 with open(args.css_prompt, "r") as file:
     CSS_PROMPT = file.read()
+with open(args.inject_script, "r") as file:
+    INJECT_SCRIPT = file.read()
 with open("index.html", "r") as file:
     INDEX_HTML = file.read()
 
@@ -203,136 +206,7 @@ def catch_all(path=""):
     if content_type == "text/html":
         response_data = response_data.replace(
             "</body>",
-            """
-            <script>
-                const loadAllSections = () => {
-                    document.querySelectorAll('[data-dynamic-content-url]').forEach(element => {
-                        if (element.hasAttribute('data-processed')) return;
-                        element.setAttribute('data-processed', 'true');
-
-                        const dynamicUrl = element.dataset.dynamicContentUrl;
-                        const tagName = element.tagName.toLowerCase();
-
-                        const postData = new FormData();
-                        Array.from(element.attributes).forEach(attr => {
-                            if (attr.name !== 'data-dynamic-content-url' && attr.name !== 'data-processed') {
-                                postData.append(attr.name, attr.value)
-                            }
-                        });
-
-                        const fetchOptions = {
-                            method: 'POST',
-                            body: postData
-                        };
-
-                        if (tagName === 'link' && element.rel === 'stylesheet') {
-                            fetch(dynamicUrl, fetchOptions)
-                                .then(res => res.text())
-                                .then(css => {
-                                    const style = document.createElement('style');
-                                    style.textContent = css;
-                                    document.head.appendChild(style);
-                                    element.remove();
-                                })
-                                .catch(console.error);
-                        } else if (tagName === 'script') {
-                            fetch(dynamicUrl, fetchOptions)
-                                .then(res => res.text())
-                                .then(js => {
-                                    const script = document.createElement('script');
-                                    script.textContent = js;
-                                    document.head.appendChild(script);
-
-                                    // Ensure the script is executed immediately after appending it to the head
-                                    const clonedScript = document.createElement('script');
-                                    clonedScript.type = 'text/javascript';
-                                    clonedScript.text = script.text;
-                                    document.body.appendChild(clonedScript);
-
-                                    element.remove();
-                                })
-                                .catch(console.error);
-                        } else {
-                            const structure = element.getAttribute('structure') || false;
-
-                            if (structure) {
-                                const standardTags = ['div', 'span', 'p'];
-                                const link = document.createElement('link');
-                                link.rel = 'stylesheet';
-                                const urlParts = dynamicUrl.split("/");
-                                const lastEmptyIndex = urlParts.lastIndexOf("");
-                                const url = urlParts[lastEmptyIndex + 1];
-                                const tagName = element.tagName.toLowerCase();
-                                const isStandardTag = standardTags.includes(tagName)
-                                const styleFileName = isStandardTag ? 'style.css' : `${tagName}.css`;
-                                link.href = `/${url}/${styleFileName}?structure=${encodeURIComponent((isStandardTag ? "" : tagName + ">" ) + structure)}`;
-                                document.head.appendChild(link);
-                            }
-
-                            element.innerHTML = '<span style="display:inline-block; opacity:0.5;">Loading content...</span>';
-                            fetch(dynamicUrl, fetchOptions)
-                                .then(res => res.text())
-                                .then(html => {
-                                    // option 1: replace element with dynamic html
-                                    //const tempContainer = document.createElement('div');
-                                    //tempContainer.innerHTML = html;
-                                    //element.replaceWith(...tempContainer.childNodes);
-
-                                    // option 2: insert dynamic html into element
-                                    element.innerHTML = html;
-
-                                    // Extract all script tags
-                                    const scripts = element.querySelectorAll('script');
-                                    scripts.forEach(script => {
-                                        const newScript = document.createElement('script');
-                                        if (script.src) {
-                                            // If the script has a src attribute, copy it and load the external script
-                                            newScript.src = script.src;
-                                            newScript.async = true;  // Preserve async behavior
-                                        } else {
-                                            // Inline script - copy the content
-                                            newScript.textContent = script.textContent;
-                                        }
-                                        document.body.appendChild(newScript);  // Append script to body to execute
-                                    });
-                                })
-                                .catch(console.error);
-                        }
-                    });
-                };
-
-                const observer = new MutationObserver((mutations) => {
-                    mutations.forEach((mutation) => {
-                        if (mutation.type === 'childList') {
-                            mutation.addedNodes.forEach((node) => {
-                                if (node.nodeType === 1) { // Check if the node is an element
-                                    // Check the node itself
-                                    if (node.hasAttribute('data-dynamic-content-url')) {
-                                        loadAllSections();
-                                    }
-                                    // Check within the subtree of the node
-                                    node.querySelectorAll('[data-dynamic-content-url]').forEach(subNode => {
-                                        if (!subNode.hasAttribute('data-processed')) {
-                                            loadAllSections();
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                });
-
-                observer.observe(document.body, {
-                    childList: true,
-                    subtree: true
-                });
-
-                document.addEventListener("DOMContentLoaded", () => {
-                    loadAllSections();
-                });
-            </script>
-        </body>
-            """
+            "<script>" + INJECT_SCRIPT + "</script></body>"
         )
 
     # save cache
